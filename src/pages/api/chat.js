10628b95be40eccd681a025003c6fa1d68de2a92
Zimbo Sebastião <gloriasebastiao7@gmail.com>
2024-01-29 16:10:24 -0300

    update

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -39,9 +39,4 @@ export default async function handler(req, res) {
   }
 }
 
-export const config = {
-  api: {
-    // disables call to body parsing module while deployed
-    bodyParser: process.env.NODE_ENV !== "production",
-  },
-};
+
